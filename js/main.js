cb420d4c24ef53d49ec44218600fc0fc9112cc34
dukicm <dukicm88@gmail.com>
2016-03-28 11:16:52 +0200

    Added files via upload

--- js/main.js
+++ js/main.js
@@ -39,20 +39,32 @@ button.addEventListener('click', validacija);
 function validacija(){
 	var ime = document.querySelector('#i').value;
 	var prezime = document.querySelector('#pre').value;
+	var email = document.querySelector('#mejl').value;
+
 	var validParagraf = document.createElement('p');
 	var validParagraf2 = document.createElement('p');
+	var validParagraf3 = document.createElement('p');
+
 	var validText = document.createTextNode("Molimo Vas da upišete Vaše ime!");
-	var validText2 = document.createTextNode("Molimo Vas da upišete Vaše prezime!")
+	var validText2 = document.createTextNode("Molimo Vas da upišete Vaše prezime!");
+	var validText3 = document.createTextNode("Molimo Vas da upišete Vašu mejl adresu!");
+	
 	var labelIme = document.querySelector('label[for="i"]');
 	var labelPrezime = document.querySelector('label[for="pre"]');
+	var labelEmail = document.querySelector('label[for="mejl"]');
+	
 	validParagraf.appendChild(validText);
 	validParagraf2.appendChild(validText2);
+	validParagraf3.appendChild(validText3);
 
 	if(ime.length < 1){
 		labelIme.appendChild(validParagraf);
 	}
 	if(prezime.length < 1){
 		labelPrezime.appendChild(validParagraf2);
+	}
+	if (email.length < 1) {
+		labelEmail.appendChild(validParagraf3);
 	}
 }
 //validacija
